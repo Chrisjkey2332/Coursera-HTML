8e17fef1ef69143c5e3480319df2c47bdeb658fc
Chrisjkey2332 <103910469+Chrisjkey2332@users.noreply.github.com>
2023-02-10 18:05:17 -0600

    testing again

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,12 +1,11 @@
 (function (window) {
-
-  var byeSpeaker = {};
+  
   var speakWord = "Good Bye";
 
-  byeSpeaker.speak = function () {
-  console.log(speakWord + " " + byeSpeaker);
-  }
-
+  var byeSpeaker = function (name) {
+    console.log(speakWord + " " + name)
+  };
+  
   window.byeSpeaker = byeSpeaker;
 
 })(window);
--- SpeakHello.js
+++ SpeakHello.js
@@ -1,10 +1,8 @@
 (function (window) {
 
-  var helloSpeaker = {};
   var speakWord = "Hello";
-
-  helloSpeaker.speak = function () {
-    console.log(speakWord + " " + helloSpeaker);
+  var helloSpeaker = function (name) {
+    console.log(speakWord + " " + name);
   }
 
   window.helloSpeaker = helloSpeaker;
